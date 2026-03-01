c1e4e0c8c4a422009bb737bf6a8b0c2de796c6b0
MarcusEcko <marco237.mnm@gmail.com>
2026-03-01 19:17:08 -0300

    improved mechanics

--- src/Components/characterCards.jsx
+++ src/Components/characterCards.jsx
@@ -8,7 +8,7 @@ export default function CharacterCards({ data, onGetScore }) {
         [...arr].sort(() => Math.random() - 0.5)
     )
 
-    ///ROWS SELECTION   
+    ///ROWS  
     const [row1, setRow1] = useState(() => shuffle(data));
     const [row2, setRow2] = useState(() => shuffle(data));
     const [selectedRow1, setSelectedRow1] = useState(null);
@@ -18,29 +18,50 @@ export default function CharacterCards({ data, onGetScore }) {
     const [score, setScore] = useState(0);
     const [highScore, setHighScore] = useState(0);
 
+    //SAVE PAIRS
+    const [matchedIDs, setMatchedIds] = useState([]);
     
-    const resetGame = () => { ///RESETS THE GAME
-        setRow1(shuffle(data));
-        setRow2(shuffle(data));
-        setSelectedRow1(null);
-        setSelectedRow2(null);
-    }
-    
-    //SHUFFLE ON SELECTION
+    //ROW SELECTION
     const handleSelectedRow1 = (id) => {
         if (selectedRow2 !== null) {
-            compareCards(id, selectedRow2);
+            compareCards(selectedRow2, id);
         } else {
             setSelectedRow1(id);
         }
+
+        if(matchedIDs.includes(id)){
+            setScore(0);
+            onGetScore(0, highScore);
+
+            setSelectedRow1(null);
+            setSelectedRow2(null);
+
+            setMatchedIds([]);
+        }
+
+        setRow1(shuffle(row1));
+        setRow2(shuffle(row2));
     };
     
     const handleSelectedRow2 = (id) => {
         if (selectedRow1 !== null) {
             compareCards(selectedRow1, id);
         } else {
-            setSelectedRow2(id);
+            setSelectedRow2(id); //no lo tendria que seleccionar si es la segunda vez que lo elige
         }
+
+        if(matchedIDs.includes(id)){
+            setScore(0);
+            onGetScore(0, highScore);
+        
+            setSelectedRow1(null);
+            setSelectedRow2(null);
+
+            setMatchedIds([]);
+        }
+
+        setRow1(shuffle(row1));
+        setRow2(shuffle(row2));
     };
     
     //COMPARE CARDS
@@ -50,18 +71,38 @@ export default function CharacterCards({ data, onGetScore }) {
         let newHighScore = highScore;
 
         if (id1 === id2) {
+
+            if(matchedIDs.includes(id1)){
+                setScore(0);
+                onGetScore(0, highScore);
+
+                setSelectedRow1(null);
+                setSelectedRow2(null);
+
+                return;
+            };
+
+
             newScore = score + 1;
             newHighScore = newScore > highScore ? newScore : highScore;
 
             setScore(newScore);
             setHighScore(newHighScore);
+
+            //save found match
+            setMatchedIds(prev => [...prev, id1]);
         } else {
             newScore = 0;
             setScore(0);
         }
 
         onGetScore(newScore, newHighScore);
-        resetGame();
+
+        setSelectedRow1(null);
+        setSelectedRow2(null);
+
+        setRow1(prev => shuffle(prev));
+        setRow2(prev => shuffle(prev));
     };
 
     return(
@@ -69,7 +110,9 @@ export default function CharacterCards({ data, onGetScore }) {
             <Row>
                 {row1.map(character => (
                     <Col key={character.id}>
-                        <Card onClick={() => handleSelectedRow1(character.id)}>
+                        <Card onClick={() =>
+                            handleSelectedRow1(character.id)}
+                        >
                             <Card.Img src={character.image}/>
                             <Card.Title>{character.name}</Card.Title>
                             <Card.Body>{character.status}</Card.Body>
@@ -80,7 +123,10 @@ export default function CharacterCards({ data, onGetScore }) {
             <Row>
                 {row2.map(character => (
                     <Col key={character.id}>
-                        <Card onClick={() => handleSelectedRow2(character.id)}>
+                        <Card onClick={() => 
+                            
+                            handleSelectedRow2(character.id)}
+                        >
                             <Card.Img src={character.image}/>
                             <Card.Title>{character.name}</Card.Title>
                             <Card.Body>{character.status}</Card.Body>
