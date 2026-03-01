e172f12219f9c16ef3f53ab54e05c431ac491d74
MarcusEcko <marco237.mnm@gmail.com>
2026-03-01 02:24:21 -0300

    card shuffle and selection added

--- src/Components/characterCards.jsx
+++ src/Components/characterCards.jsx
@@ -1,51 +1,66 @@
 import { useState } from "react";
+import { Card, Col, Container, Row } from "react-bootstrap";
 
 export default function CharacterCards({ data }) {
 
-    const shuffle = (arr) =>
-        [...arr].sort(() => Math.random() - 0.5);
+    ///SORT SHUFFLE FUNCTION
+    const shuffle = (arr) => (
+        [...arr].sort(() => Math.random() - 0.5)
+    )
 
+    ///ROWS SELECTION   
     const [row1, setRow1] = useState(() => shuffle(data));
     const [row2, setRow2] = useState(() => shuffle(data));
     const [selectedRow1, setSelectedRow1] = useState(null);
     const [selectedRow2, setSelectedRow2] = useState(null);
-
-    const resetGame = () => {
+    
+    const resetGame = () => { ///RESETS THE GAME
         setRow1(shuffle(data));
         setRow2(shuffle(data));
         setSelectedRow1(null);
         setSelectedRow2(null);
-    };
-
+    }
+    
     const handleSelectedRow1 = (id) => {
         setSelectedRow1(id);
 
-        if (selectedRow2 !== null) {
+        if(selectedRow2 !== null){
             resetGame();
         }
-    };
+    }
 
     const handleSelectedRow2 = (id) => {
         setSelectedRow2(id);
 
-        if (selectedRow1 !== null) {
+        if(selectedRow1 !== null){
             resetGame();
         }
-    };
+    }
 
-    return (
-        <>
-            {row1.map(item => (
-                <div key={item.id} onClick={() => handleSelectedRow1(item.id)}>
-                    {item.name}
-                </div>
-            ))}
-
-            {row2.map(item => (
-                <div key={item.id} onClick={() => handleSelectedRow2(item.id)}>
-                    {item.name}
-                </div>
-            ))}
-        </>
-    );
+    return(
+        <Container>
+            <Row>
+                {row1.map(character => (
+                    <Col>
+                        <Card onClick={() => handleSelectedRow1(character.id)}>
+                            <Card.Img src={character.image}/>
+                            <Card.Title>{character.name}</Card.Title>
+                            <Card.Body>{character.status}</Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+            <Row>
+                {row2.map(character => (
+                    <Col>
+                        <Card onClick={() => handleSelectedRow2(character.id)}>
+                            <Card.Img src={character.image}/>
+                            <Card.Title>{character.name}</Card.Title>
+                            <Card.Body>{character.status}</Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </Container>
+    )
 }
